3d9eef941aff40fd5147b779cbe1c60fc4a76371
malegarcal <mgarciac@outlook.it>
2021-03-04 22:57:22 -0300

    created structure and add funtionality

---
+++ data.js
@@ -0,0 +1,40 @@
+let threads = [
+    {
+        id: 1,
+        title: "Thread 1",
+        author: "Manuel",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "Alejandro",
+                date: Date.now(),
+                content: "Hey there"
+            },
+            {
+                author: "Manuel",
+                date: Date.now(),
+                content: "Hey to you too"
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Thread 2",
+        author: "Alejandro",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "Alejandro",
+                date: Date.now(),
+                content: "Hey there"
+            },
+            {
+                author: "Manuel",
+                date: Date.now(),
+                content: "Hey to you too"
+            }
+        ]
+    }
+]
